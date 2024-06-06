d4b6242c8e20c1cb12a8d1d779813b72e260ad88
F1B1 <F1B1>
2024-06-07 00:30:39 +0300

    six

--- src/js/index.js
+++ src/js/index.js
@@ -23,17 +23,30 @@ document.addEventListener('DOMContentLoaded',()=>{
     const menu = document.querySelector('.menu');
     const menuClose = document.querySelector('.menu-close');
 
+    function closeMenu(e) {
+        if (e.target === menuClose || (!menu.contains(e.target) && !e.target.closest('.menu-open'))) {
+            menu.classList.remove('active');
+            document.body.classList.remove('block-scroll');
+            document.removeEventListener('click', closeMenu);  
+        }
+    }
+    
     menuOpen.forEach(openmenu => {
         openmenu.addEventListener("click", function(e) {
-            menu.classList.add('active')
-            document.body.classList.add('block-scroll')
+            menu.classList.add('active');
+            document.body.classList.add('block-scroll');
+            document.addEventListener('click', closeMenu); 
         });
     });
-
+    
     menuClose.addEventListener("click", function(e) {
-        menu.classList.remove('active')
-        document.body.classList.remove('block-scroll')
+        menu.classList.remove('active');
+        document.body.classList.remove('block-scroll');
+        document.removeEventListener('click', closeMenu); 
     });
+
+
+    
 
     const cityOpen =document.querySelectorAll('.city-open');
     const cityModal = document.querySelector('.city__popup');
